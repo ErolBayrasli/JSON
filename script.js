7bdaa35b6117ce63f716a5e3d28be1975ce5ffdd
Erol <126ebayrasli@frhsd.com>
2026-01-16 08:36:55 -0500

    done

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
  const xhr = new XMLHttpRequest();
 
         xhr.onload = function(){
-           if(xhr.status === 200){
-                const data= JSON.parse(xhr.responseText);
-           }
-        };
+           
+            if(xhr.status === 200){
+                const data = JSON.parse(xhr.responseText);
+                const players = data.players;
+                const container = document.getElementById("cards");
+               
+                
+                players.forEach(player => {
+                    const card = document.createElement('div');
+                    let statsHtml ="";
+                    let teamsHtml ="";
+                    let awardsHtml ="";
+
+                    for (let i = 0; i < player.teams.length; i++) {
+                        
+                    
+                }
+
+                });
+
+                
+
+             
+                
+                    console.log(players);
+            }
+        }
+
         xhr.open("GET", "player.json");
         xhr.send();
